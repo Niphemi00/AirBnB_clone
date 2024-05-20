db170952129e30c199f677533b46f1ef47d2a7ce
Niphemi00 <134238348+Niphemi00@users.noreply.github.com>
2024-05-20 10:24:29 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""
-Module for the Amenity class.
-"""
+"""This module creates a Amenity class"""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """
-    Represent an amenity.
-
-    Attributes:
-        name (str): The name of the amenity, wifi and any other amenity
-    """
+    """Class for managing amenity objects"""
 
     name = ""
+    
